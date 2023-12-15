3afd5cfbe5da7343f840e2840df4c8f49c0f8b52
costarelli marco <costarelli.marco@iisgalvanimi.edu.it>
2023-12-15 10:38:07 +0000

    funziona tutto

---
+++ introduzione_angular_1/costa_natale/angular/src/app/fourth-component/fourth-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fourth-component',
+  templateUrl: './fourth-component.component.html',
+  styleUrls: ['./fourth-component.component.css']
+})
+export class FourthComponentComponent {
+  title = 'angular';
+  name = "quarto";
+  TextColor: string = 'yellow';
+}
---
+++ introduzione_angular_1/costa_natale/angular/src/app/third-component/third-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-third-component',
+  templateUrl: './third-component.component.html',
+  styleUrls: ['./third-component.component.css']
+})
+export class ThirdComponentComponent {
+  title = 'angular';
+  name = "terzo";
+  TextColor: string = 'green';
+}
